4c5cd50a48dbf1283bc4de93d27171eb5f1ff4e4
Stanghi <stangherlin.matti@gmail.com>
2023-01-13 12:38:06 +0100

    fix layout card

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,27 +2,29 @@
 
 @section('content')
     <div class="container">
-        <div class="row row-cols-6">
+        <div class="row row-cols-2 row-cols-md-4 row-cols-lg-6">
             @forelse ($comics as $comic)
                 <div class="col p-3">
                     <div class="card">
                         <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
-                        <div class="card-body">
-                            <p class="card-text">{{ $comic->title }}</p>
-                        </div>
-                        <div class="d-flex justify-content-around mb-3">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <p class="card-text fw-bold">{{ $comic->title }}</p>
 
-                            <a href="{{ route('comics.show', $comic) }}" title="Show" class="btn btn-outline-primary">
-                                <i class="fa-solid fa-eye"></i>
-                            </a>
+                            <div class="d-flex justify-content-around">
 
-                            <a href="#" title="Edit" class="btn btn-outline-primary"><i class="fa-solid fa-pen"></i>
-                            </a>
+                                <a href="{{ route('comics.show', $comic) }}" title="Show" class="btn btn-outline-primary">
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
 
-                            <a href="#" title="Delete" class="btn btn-outline-danger"><i
-                                    class="fa-solid fa-trash"></i>
-                            </a>
+                                <a href="#" title="Edit" class="btn btn-outline-primary"><i
+                                        class="fa-solid fa-pen"></i>
+                                </a>
 
+                                <a href="#" title="Delete" class="btn btn-outline-danger"><i
+                                        class="fa-solid fa-trash"></i>
+                                </a>
+
+                            </div>
                         </div>
                     </div>
                 </div>
